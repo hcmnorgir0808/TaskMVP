cdc84d12174c5e4a6df12ffe6742f6b176944075
mocchan <hcmnorgir0808@yahoo.co.jp>
2021-05-09 19:09:51 +0900

    prepareForReuseではViewの更新をしない

--- TaskMVP/Search/View/MVPTableViewCell.swift
+++ TaskMVP/Search/View/MVPTableViewCell.swift
@@ -11,19 +11,12 @@ import UIKit
  MVCと基本変わらない
  */
 final class MVPTableViewCell: UITableViewCell {
-
-  @IBOutlet private weak var titleLabel: UILabel!
-  @IBOutlet private weak var urlLabel: UILabel!
-
-  override func prepareForReuse() {
-    super.prepareForReuse()
-    titleLabel.text = nil
-    urlLabel.text = nil
-  }
-
-  func configure(githubModel: GithubModel) {
-    self.titleLabel.text = githubModel.fullName
-    self.urlLabel.text = githubModel.urlStr
-  }
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var urlLabel: UILabel!
+    
+    func configure(githubModel: GithubModel) {
+        self.titleLabel.text = githubModel.fullName
+        self.urlLabel.text = githubModel.urlStr
+    }
 }
-
